3a1316790c8bb6ef073ed801925f524a1924d4bf
anton <anton.menkveld@ecocentric.energy>
2019-06-08 07:07:29 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <Toolbar />
+    <nuxt />
+</div>
+</template>
+
+<script>
+import Toolbar from '~/components/core/Toolbar'
+export default {
+    components: {
+        Toolbar
+    }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  head: {
+    title: 'Numen Installer',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: pkg.description}
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  loading: {color: '#BDBDBD', height: '3px'},
+
+  css: ['~/assets/style/app.styl'],
+
+  plugins: ['@/plugins/vuetify.js'],
+
+  modules: ['@nuxtjs/axios'],
+
+  axios: {},
+
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    }
+  }
+}
